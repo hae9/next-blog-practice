ab71beec49305bcb392224662ca5c064d7effc4f
hae9 <gorud0929@naver.com>
2023-04-19 22:45:46 +0900

    Revert "Feat: 블로그 카테고리 까지"
    This reverts commit c306b26b91ccfecaf367fdfa9caad39be4a3bdbc.

--- src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return <footer className="bg-slate-900 text-white py-2 text-sm text-center">{`Hae9's Blog | All Rights Reserved`}</footer>;
-}
